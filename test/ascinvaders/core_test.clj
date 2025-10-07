708a2157382e7fb7a8674e405a94e4630ae6d590
Ovi Stoica <ovidiu.stoica1094@gmail.com>
2025-10-07 16:35:12 +0300

    Initial commit setting things up

---
+++ test/ascinvaders/core_test.clj
@@ -0,0 +1,8 @@
+(ns ascinvaders.core-test
+  (:require
+   [ascinvaders.core :as core]
+   [clojure.test :refer [deftest is testing]]))
+
+(deftest default-test
+  (testing "I don't do much"
+    (is true)))
